60baffbd4af12afdd9d02d70c6b535ecceac0e3f
Ashley Fairweather <shuriishurii@gmail.com>
2016-12-29 21:53:08 -0500

    Parallax with temporary background assets.

---
+++ ProjectL/Assets/Scripts/ParallaxScroller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxScroller : MonoBehaviour
+{
+    [SerializeField]
+    private float speedMultiplier = 1;
+    private Renderer _renderer;
+
+    void Awake()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        float offset = Time.time * (0.15f * speedMultiplier);
+        _renderer.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+}
